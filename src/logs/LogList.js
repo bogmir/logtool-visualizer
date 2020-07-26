193450374062f5fb741126b7178f151f479530e6
Bogdan Mircea <68702939+bogmir@users.noreply.github.com>
2020-07-26 10:36:47 +0200

    first pie

--- src/logs/LogList.js
+++ src/logs/LogList.js
@@ -1,23 +1,40 @@
 import React, {Component} from 'react';
-import LogData from '../data/epa-http-small.json';
+import methodRequests from '../data/LogData';
+import {PieChart, Pie, Cell, Legend, Tooltip} from 'recharts';
+import {generateComplementaryColors} from '@stephencweiss/generate-complementary-colors';
+import rca, {pastel} from 'rainbow-colors-array';
+
+
+const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
+                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
+
+const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
+                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
+                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
+
 
 class LogList extends Component {
     render() {
+      const colors = rca(data01.length, "hex", pastel).map(a => '#'+a.hex);
         return (
             <div>
-              <h1>Hello, man</h1>
-              {LogData.map(((logLine, index) => {
-                  return (
-                      <div>
-                        <h1>{logLine.host}</h1>
-                        <p>{logLine.datetime.day}:{logLine.datetime.hour}:{logLine.datetime.minute}:{logLine.datetime.second}</p>
-                      </div>
-                    );
-              }))
-              }
+              <PieChart width={800} height={400}>
+                <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label>
+                {
+      data01.map((entry, index) => (
+        <Cell key={`cell-${index}`} fill={colors[index]}/>
+      ))
+    }
+    </Pie>
+ {/*                <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+  */}               <Tooltip/>
+              </PieChart>
+
             </div>
           );
     }
 }
+
 
 export default LogList;
